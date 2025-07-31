b5ed1fa003505ff16e899cd2947b3bf64979babb
Jayantkumar Tiwari <158484490+JayTiwari2603@users.noreply.github.com>
2025-07-31 14:10:27 +0530

    Update DBUtil.java

--- src/main/java/com/company/util/DBUtil.java
+++ src/main/java/com/company/util/DBUtil.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-	 private static final String URL = "jdbc:mysql://localhost:3306/registerlogin";
-	 private static final String USERNAME = "root";
-	 private static final String PASSWORD = "Jay#26@1234";
+	 private static final String URL = "jdbc:mysql://localhost:3306/your_db";
+         private static final String USERNAME = "your_username";
+         private static final String PASSWORD = "your_password";
+
     static {
         try {
             // Load the MySQL JDBC driver
