90715fa1f6b27fb0293075e830c3aa3e0aee007a
Jayantkumar Tiwari <jayantiwari2603@gmail.com>
2025-07-31 13:30:59 +0530

    Initial commit - Java Servlet project

---
+++ src/main/java/com/company/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.company.dao;
+//
+//import com.company.model.User;
+
+public interface UserDao {
+
+//	boolean addUser(User user);
+	boolean isValidUser(String username, String password);
+
+
+	boolean addUser(User user);
+}
